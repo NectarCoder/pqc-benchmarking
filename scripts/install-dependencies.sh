a422938a38c189aa6ccd1829f7eebfd721bd5b4f
Nectar Coder <109771600+NectarCoder@users.noreply.github.com>
2025-09-12 15:04:58 -0400

    Added generator files that can be timed
    - some spacing updates to the scripts

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+
 # For Ubuntu/Debian based systems using the apt package manager
-# Installs packages required for building liboqs, OpenSSL, and the provider
+# Installs dependencies required for building OpenSSL, liboqs, and oqs-provider
 
 echo -e "\033[1;31mIMPORTANT:\033[0m sudo access is required to install dependencies."
 echo "You may be prompted for your password."
 sudo apt update
-sudo apt install -y build-essential cmake git ninja-build
+sudo apt install -y build-essential cmake git ninja-build python3 python-is-python3 python3-pip curl wget
+
+# List of dependencies
+# - build-essential
+# - cmake
+# - git
+# - ninja-build
+# - python3
+# - python-is-python3
+# - python3-pip
+# - curl
+# - wget
