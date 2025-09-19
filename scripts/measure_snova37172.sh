fdb7f07240ceec92b217b5b3c160323439416288
Nectar Coder <109771600+NectarCoder@users.noreply.github.com>
2025-09-19 18:37:00 -0400

    Added SNOVA

---
+++ scripts/measure_snova37172.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Specify results and generated directories
+export TIMING_RESULTS_DIR=results/snova37172_times
+export MEMORY_RESULTS_DIR=results/snova37172_memory
+export GENERATED_DIR=results/snova37172_generated
+
+# Commands used to generate keys, CSRs, and certificates using OpenSSL via oqs-provider
+export GEN_CMD="./openssl genpkey -provider default -provider oqsprovider -algorithm snova37172 -out $GENERATED_DIR/private.key"
+export PUB_CMD="./openssl pkey -in $GENERATED_DIR/private.key -pubout -out $GENERATED_DIR/public.key -provider default -provider oqsprovider"
+export CSR_CMD="./openssl req -new -key $GENERATED_DIR/private.key -out $GENERATED_DIR/request.csr -subj \"/C=US/ST=Georgia/L=Atlanta/O=My Company Inc/CN=mycompany.com\" -provider default -provider oqsprovider"
+export SIGN_CMD="./openssl x509 -req -days 365 -in $GENERATED_DIR/request.csr -signkey $GENERATED_DIR/private.key -out $GENERATED_DIR/certificate.crt -provider default -provider oqsprovider"
+export VERIFY_CMD="./openssl verify -provider default -provider oqsprovider -CAfile $GENERATED_DIR/certificate.crt $GENERATED_DIR/certificate.crt"
+
+# Call the metrics calculation script
+eval "$(dirname "$0")/record_metrics.sh"
