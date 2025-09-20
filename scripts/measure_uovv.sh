651c654213eac338900dc305954984937f128843
Nectar Coder <109771600+NectarCoder@users.noreply.github.com>
2025-09-19 20:53:31 -0400

    Added UOV

--- scripts/measure_uovv.sh
+++ scripts/measure_uovv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Specify results and generated directories
-export TIMING_RESULTS_DIR=results/snova49113_times
-export MEMORY_RESULTS_DIR=results/snova49113_memory
-export GENERATED_DIR=results/snova49113_generated
+export TIMING_RESULTS_DIR=results/uovv_times
+export MEMORY_RESULTS_DIR=results/uovv_memory
+export GENERATED_DIR=results/uovv_generated
 
 # Commands used to generate keys, CSRs, and certificates using OpenSSL via oqs-provider
-export GEN_CMD="./openssl genpkey -provider default -provider oqsprovider -algorithm snova49113 -out $GENERATED_DIR/private.key"
+export GEN_CMD="./openssl genpkey -provider default -provider oqsprovider -algorithm OV_V -out $GENERATED_DIR/private.key"
 export PUB_CMD="./openssl pkey -in $GENERATED_DIR/private.key -pubout -out $GENERATED_DIR/public.key -provider default -provider oqsprovider"
 export CSR_CMD="./openssl req -new -key $GENERATED_DIR/private.key -out $GENERATED_DIR/request.csr -subj \"/C=US/ST=Georgia/L=Atlanta/O=My Company Inc/CN=mycompany.com\" -provider default -provider oqsprovider"
 export SIGN_CMD="./openssl x509 -req -days 365 -in $GENERATED_DIR/request.csr -signkey $GENERATED_DIR/private.key -out $GENERATED_DIR/certificate.crt -provider default -provider oqsprovider"
