f141e2ba3bc448818c8ade17b90bc49420c3586d
Nectar Coder <109771600+NectarCoder@users.noreply.github.com>
2025-10-11 00:48:49 -0400

    Updated benchmarking code to include mirath
    - Updated file perms and removed ryde1f testing

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -42,12 +42,15 @@ if __name__ == "__main__":
     # Run benchmarks for each algorithm
     # Arguments: (output_csv, prefix, [algorithm id, algorithm name, NIST security level, private key size, public key size, signature size])
 
+    # DONE
     # run_benchmark(out_csv, 'rsa',        ['rsa', 'RSA 2048', 'N/A','1190','294','256'])
 
+    # DONE
     # run_benchmark(out_csv, 'dilithium2', ['mldsa44','ML-DSA/Dilithium 2', '2','2560','1312','2420'])
     # run_benchmark(out_csv, 'dilithium3', ['mldsa65','ML-DSA/Dilithium 3', '3','4032','1952','3309'])
     # run_benchmark(out_csv, 'dilithium5', ['mldsa87','ML-DSA/Dilithium 5', '5','4896','2592','4627'])
 
+    # NOT DONE
     # run_benchmark(out_csv, 'sphincssha1s', ['slh-dsa-sha2-128s','SLH-DSA/SPHINCS+ SHA 128s', '1','64','32','7856'])
     # run_benchmark(out_csv, 'sphincssha3s', ['slh-dsa-sha2-192s','SLH-DSA/SPHINCS+ SHA 192s', '3','96','48','16224'])
     # run_benchmark(out_csv, 'sphincssha5s', ['slh-dsa-sha2-256s','SLH-DSA/SPHINCS+ SHA 256s', '5','128','64','29792'])
@@ -55,14 +58,17 @@ if __name__ == "__main__":
     # run_benchmark(out_csv, 'sphincsshake3s', ['slh-dsa-shake-192s','SLH-DSA/SPHINCS+ SHAKE 192s', '3','96','48','16224'])
     # run_benchmark(out_csv, 'sphincsshake5s', ['slh-dsa-shake-256s','SLH-DSA/SPHINCS+ SHAKE 256s', '5','128','64','29792'])
 
+    # DONE
     # run_benchmark(out_csv, 'falcon512', ['falcon512','FALCON 512','1','1281','897','666'])
     # run_benchmark(out_csv, 'falcon1024', ['falcon1024','FALCON 1024','5','2305','1793','1280'])
 
+    # DONE
     # run_benchmark(out_csv, 'mayo1', ['mayo1','MAYO 1','1','24','1420','454'])
     # run_benchmark(out_csv, 'mayo2', ['mayo2','MAYO 2','1','24','4912','186'])
     # run_benchmark(out_csv, 'mayo3', ['mayo3','MAYO 3','3','32','2986','681'])
     # run_benchmark(out_csv, 'mayo5', ['mayo5','MAYO 5','5','40','5554','964'])
 
+    # NOT DONE
     # run_benchmark(out_csv, 'crossrsdp1s', ['CROSSrsdp128small','CROSS-RSDP-128-Small','1','32','77','12432'])
     # run_benchmark(out_csv, 'crossrsdp3s', ['CROSSrsdp192small','CROSS-RSDP-192-Small','3','48','115','28391'])
     # run_benchmark(out_csv, 'crossrsdp5s', ['CROSSrsdp256small','CROSS-RSDP-256-Small','5','64','153','50818'])
@@ -70,6 +76,7 @@ if __name__ == "__main__":
     # run_benchmark(out_csv, 'crossrsdpg3s', ['CROSSrsdpg192small','CROSS-RSDP-G-192-Small','3','48','83','20452'])
     # run_benchmark(out_csv, 'crossrsdpg5s', ['CROSSrsdpg256small','CROSS-RSDP-G-256-Small','5','64','106','36454'])
 
+    # NOT DONE
     # run_benchmark(out_csv, 'snova2454', ['snova2454','SNOVA-24-5-4','1','48','1016','248'])
     # run_benchmark(out_csv, 'snova37172', ['snova37172','SNOVA-37-17-2','1','48','9842','124'])
     # run_benchmark(out_csv, 'snova2583', ['snova2583','SNOVA-25-8-3','1','48','2320','165'])
@@ -82,14 +89,17 @@ if __name__ == "__main__":
     # run_benchmark(out_csv, 'snova66153', ['snova66153','SNOVA-66-15-3','5','48','15204','381'])
     # run_benchmark(out_csv, 'snova75332', ['snova75332','SNOVA-75-33-2','5','48','71890','232'])
 
+    # DONE
     # run_benchmark(out_csv, 'uovis', ['OV_Is', 'UOV-Is', '1', '760892', '412181', '96'])
     # run_benchmark(out_csv, 'uovip', ['OV_Ip', 'UOV-Ip', '1', '516356', '278453', '128'])
     # run_benchmark(out_csv, 'uoviii', ['OV_III', 'UOV-III', '3', '2269788', '1225461', '200'])
     # run_benchmark(out_csv, 'uovv', ['OV_V', 'UOV-V', '5', '5306172', '2869461', '260'])
 
+    # DONE
     # run_benchmark(out_csv, 'hawk512', ['hawk512','HAWK 512','1','184','1024','555'])
     # run_benchmark(out_csv, 'hawk1024', ['hawk1024','HAWK 1024','5','360','2440','1221'])
     
+    # NOT DONE
     # run_benchmark(out_csv, 'faest128s', ['faest128s','FAEST 128s','1','32','32','4506'])
     # run_benchmark(out_csv, 'faest192s', ['faest192s','FAEST 192s','3','40','48','11260'])
     # run_benchmark(out_csv, 'faest256s', ['faest256s','FAEST 256s','5','48','48','20696'])
@@ -97,13 +107,48 @@ if __name__ == "__main__":
     # run_benchmark(out_csv, 'faestem192s', ['faestem192s','FAEST-EM 192s','3','48','48','9340'])
     # run_benchmark(out_csv, 'faestem256s', ['faestem256s','FAEST-EM 256s','5','64','64','17984'])
 
-    run_benchmark(out_csv, 'perkak1short', ['perkak1short','PERK-AES-KECCAK-1-short','1','120','104','3473'])
-    run_benchmark(out_csv, 'perkak3short', ['perkak3short','PERK-AES-KECCAK-3-short','3','175','151','8311'])
-    run_benchmark(out_csv, 'perkak5short', ['perkak5short','PERK-AES-KECCAK-5-short','5','227','195','14830'])
+    # NOT DONE
+    # run_benchmark(out_csv, 'perkak1short', ['perkak1short','PERK-AES-KECCAK-1-short','1','120','104','3473'])
+    # run_benchmark(out_csv, 'perkak3short', ['perkak3short','PERK-AES-KECCAK-3-short','3','175','151','8311'])
+    # run_benchmark(out_csv, 'perkak5short', ['perkak5short','PERK-AES-KECCAK-5-short','5','227','195','14830'])
 
+    # NOT DONE
     # run_benchmark(out_csv, 'ryde1s', ['ryde1s','RYDE 1-Short','1','32','69','3115'])
     # run_benchmark(out_csv, 'ryde3s', ['ryde3s','RYDE 3-Short','3','48','101','7064'])
     # run_benchmark(out_csv, 'ryde5s', ['ryde5s','RYDE 5-Short','5','64','132','12607'])
+
+    """
+    Parameter Set	NIST Security Level	SK size	PK size	Sig size
+    Mirath-1a-Short	1	32 B	73 B	3 182 B
+    Mirath-1b-Short	1	32 B	57 B	2 990 B
+    Mirath-3a-Short	3	48 B	107 B	7 456 B
+    Mirath-3b-Short	3	48 B	84 B	6 825 B
+    Mirath-5a-Short	5	64 B	147 B	13 091 B
+    Mirath-5b-Short	5	64 B	112 B	12 229 B
+    """
+    # NOT DONE
+    # run_benchmark(out_csv, 'mirath1ashort', ['mirathtcith1ashort','MIRATH-TCITH-1a-Short','1','32','73','3182'])
+    # run_benchmark(out_csv, 'mirath1bshort', ['mirathtcith1bshort','MIRATH-TCITH-1b-Short','1','32','57','2990'])
+    # run_benchmark(out_csv, 'mirath3ashort', ['mirathtcith3ashort','MIRATH-TCITH-3a-Short','3','48','107','7456'])
+    # run_benchmark(out_csv, 'mirath3bshort', ['mirathtcith3bshort','MIRATH-TCITH-3b-Short','3','48','84','6825'])
+    # run_benchmark(out_csv, 'mirath5ashort', ['mirathtcith5ashort','MIRATH-TCITH-5a-Short','5','64','147','13091'])
+    # run_benchmark(out_csv, 'mirath5bshort', ['mirathtcith5bshort','MIRATH-TCITH-5b-Short','5','64','112','12229'])
+
+
+    """
+    TODO SDitH
+    Sec Level 1
+    Instance	Public Key (bytes)	Secret Key (bytes)	Signature (bytes)
+    SDitH2-L1-gf2-short	70	163	3705
+
+    Sec Level 3
+    Instance	Public Key (bytes)	Secret Key (bytes)	Signature (bytes)
+    SDitH2-L3-gf2-short	98	232	7964
+
+    Sec Level 5
+    Instance	Public Key (bytes)	Secret Key (bytes)	Signature (bytes)
+    SDitH2-L5-gf2-short	132	307	14121
+    """
 
     print("\n\033[92mBenchmarking process complete. Results saved to 'results/benchmarks.csv'.\033[0m")
     
@@ -115,10 +160,10 @@ if __name__ == "__main__":
     # run_benchmark(out_csv, 'sphincsshake3f', ['slh-dsa-shake-192f','SPHINCS+ SHAKE 192f', '3','116','66','35664'])
     # run_benchmark(out_csv, 'sphincsshake5f', ['slh-dsa-shake-256f','SPHINCS+ SHAKE 256f', '5','150','82','49856'])
 
-    # We are not testing the "fast" variants of PERK
+    # We are not testing the "fast" variants of PERK (will not work if uncommented)
     # run_benchmark(out_csv, 'perk128fast3', ['perk128fast3','PERK 128 Fast 3 Iterations','1','164','148','8345'])
 
-    # We are not testing the "fast" variants of RYDE
+    # We are not testing the "fast" variants of RYDE (will not work if uncommented)
     # run_benchmark(out_csv, 'ryde1f', ['ryde1f','RYDE 1-Fast','1','32','69','3597'])
 
     # We are not testing these depreceated versions of perk (these will not work if uncommented)
--- scripts/measure_mirath5ashort.sh
+++ scripts/measure_mirath5ashort.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
 # Specify results and generated directories
-export TIMING_RESULTS_DIR=results/faestem192s_times
-export MEMORY_RESULTS_DIR=results/faestem192s_memory
-export GENERATED_DIR=results/faestem192s_generated
+export TIMING_RESULTS_DIR=results/mirath5ashort_times
+export MEMORY_RESULTS_DIR=results/mirath5ashort_memory
+export GENERATED_DIR=results/mirath5ashort_generated
 
 # Commands used to generate keys, CSRs, and certificates using OpenSSL via oqs-provider
-export GEN_CMD="./openssl genpkey -provider default -provider oqsprovider -algorithm faestem192s -out $GENERATED_DIR/private.key"
+export GEN_CMD="./openssl genpkey -provider default -provider oqsprovider -algorithm mirathtcith5ashort -out $GENERATED_DIR/private.key"
 export PUB_CMD="./openssl pkey -in $GENERATED_DIR/private.key -pubout -out $GENERATED_DIR/public.key -provider default -provider oqsprovider"
 export CSR_CMD="./openssl req -new -key $GENERATED_DIR/private.key -out $GENERATED_DIR/request.csr -subj \"/C=US/ST=Georgia/L=Atlanta/O=My Company Inc/CN=mycompany.com\" -provider default -provider oqsprovider"
 export SIGN_CMD="./openssl x509 -req -days 365 -in $GENERATED_DIR/request.csr -signkey $GENERATED_DIR/private.key -out $GENERATED_DIR/certificate.crt -provider default -provider oqsprovider"
